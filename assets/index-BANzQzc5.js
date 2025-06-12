6a6126d8150e821951531013446ce4b6495d3903
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2025-06-12 22:11:05 +0800

    feat: update

--- assets/index-BANzQzc5.js
+++
@@ -1,1 +0,0 @@
-import{b as o}from"./route-block-B_A1xBdJ.js";import{M as n,N as r,o as a,O as s,ah as c,Q as f}from"./index-C8UyTDqa.js";const t={};function i(l,e){return a(),r(f,null,{default:s(()=>e[0]||(e[0]=[c(" Home ")])),_:1})}typeof o=="function"&&o(t);const m=n(t,[["render",i]]);export{m as default};
--- assets/plain-H-Jcq0Ti.js
+++
@@ -1,1 +0,0 @@
-import{d as t,c as r,a as e,r as _,_ as a,F as s,o as c}from"./index-C8UyTDqa.js";const i=t({__name:"plain",setup(p){return(m,l)=>{const o=_("router-view"),n=a;return c(),r(s,null,[e(o),e(n)],64)}}});export{i as default};
