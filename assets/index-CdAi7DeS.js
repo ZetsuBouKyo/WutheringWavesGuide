550d15325a47ab4ed9147b18b57f1febafe846cc
ZetsuBouKyo <116266262+ZetsuBouKyo@users.noreply.github.com>
2025-08-29 23:09:12 +0800

    feat: update

---
+++ assets/index-CdAi7DeS.js
@@ -0,0 +1,1 @@
+import{b as o}from"./route-block-B_A1xBdJ.js";import{b as n,e as r,o as a,w as s,ah as c,V as f}from"./index-qc7TtUQv.js";const t={};function i(l,e){return a(),r(f,null,{default:s(()=>e[0]||(e[0]=[c(" Home ")])),_:1})}typeof o=="function"&&o(t);const m=n(t,[["render",i]]);export{m as default};
